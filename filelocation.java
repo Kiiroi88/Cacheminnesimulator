b07f1f30e25ddb786d11283fff2c524d16851ad9
Magnus Koch <magnus.koch88@gmail.com>
2013-04-12 13:20:23 +0200

    Säger till fileReader vart filen finns
    Just nu statisk address

---
+++ filelocation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cachesimulator;
+
+/**
+ *
+ * @author Magnus
+ */
+public class filelocation {
+  
+    public String fileLocation(){
+        String y = "C:\\Users\\Magnus\\Documents\\NetBeansProjects\\testCachesim\\build\\classes\\testcachesim\\test.txt";
+        return y;
+    }
+}
+    
